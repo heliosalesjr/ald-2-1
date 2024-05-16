acc49b26a65f8b6b405da4bc66bfa569f3d4ec5a
heliosalesjr <heliosalesjr@gmail.com>
2024-05-16 20:17:52 -0300

    1st commit: here we go!

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <>
+      <h1 className='text-center text-3xl font-semibold p-16'>Here we go!</h1>
+    </>
+  )
+}
+
+export default Home
