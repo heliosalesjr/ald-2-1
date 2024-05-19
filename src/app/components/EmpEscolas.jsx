e10790944e4ebafe8e0786874251ce87df089b6f
heliosalesjr <heliosalesjr@gmail.com>
2024-05-19 18:45:24 -0300

    Another main page component done

--- src/app/components/EmpEscolas.jsx
+++ src/app/components/EmpEscolas.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import Image from 'next/image';
+import backgroundImage from '/public/images/esc1.jpg'; // Ajuste o caminho da imagem conforme necessário
 
 function EmpEscolas() {
   return (
-    <div>EmpEscolas</div>
-  )
+    <div className="relative w-full h-[800px]">
+      <div className="absolute inset-0">
+        <Image 
+          src={backgroundImage}
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+          className="w-full h-full"
+        />
+      </div>
+      <div className="absolute inset-0 flex">
+        <div className="w-full md:w-1/2 bg-black bg-opacity-50 flex flex-col justify-center p-8">
+          <h1 className="text-white underline decoration-primary text-2xl md:text-4xl mb-4 font-semibold pt-8">
+          Por que Empreendedorismo nas escolas?
+
+          </h1>
+          <p className="text-white py-2">
+          A nova BNCC propõe “a educação integral dos estudantes com a construção de seu projeto de vida e de uma cultura favorável ao desenvolvimento de atitudes, capacidades e valores que promovam o Empreendedorismo (criatividade, inovação, organização, planejamento, responsabilidade, liderança, colaboração, visão de futuro, assunção de riscos, resiliência e curiosidade científica, entre outros), entendido como competência essencial ao desenvolvimento pessoal, à cidadania ativa, à inclusão social e à empregabilidade”.
+
+          </p>
+          <p className="text-white py-2">
+          Os itinerários formativos da nova BNCC, organizados em torno de eixos estruturantes, permitem a apropriação de conteúdos que favoreçam o protagonismo juvenil, a capacidade de desenvolver pensamento crítico e consciente em um mundo em constante mudança. Neste programa, o Empreendedorismo combina as áreas de Matemática e Ciências Humanas e enseja o desenvolvimento das competências gerais.
+
+          </p>
+        </div>
+        <div className="md:w-1/2"></div>
+      </div>
+    </div>
+  );
 }
 
-export default EmpEscolas
+export default EmpEscolas;
--- src/app/components/QueEmpreendedorismo.jsx
+++ src/app/components/QueEmpreendedorismo.jsx
@@ -17,11 +17,11 @@ function QueEmpreendedorismo() {
       <div className="absolute inset-0 flex">
         <div className=" md:w-1/2"></div>
         <div className="w-full md:w-1/2 bg-black bg-opacity-50 flex flex-col justify-center p-8">
-          <h1 className="text-white text-2xl md:text-4xl mb-4 font-semibold">O que é Empreendedorismo?
+          <h1 className="text-white underline decoration-primary text-2xl md:text-4xl mb-4 font-semibold pt-8">O que é Empreendedorismo?
 </h1>
-          <p className="text-white md:text-lg py-2">Segundo o Sebrae, Empreendedorismo é “a capacidade que uma pessoa tem de identificar problemas e oportunidades, desenvolver soluções inovadoras e investir recursos na criação de algo positivo para a sociedade. Pode ser um negócio, um projeto ou mesmo um movimento que gere mudanças reais e impacto no cotidiano das pessoas”.
+          <p className="text-white py-2">Segundo o Sebrae, Empreendedorismo é “a capacidade que uma pessoa tem de identificar problemas e oportunidades, desenvolver soluções inovadoras e investir recursos na criação de algo positivo para a sociedade. Pode ser um negócio, um projeto ou mesmo um movimento que gere mudanças reais e impacto no cotidiano das pessoas”.
 </p>
-          <p className="text-white md:text-lg py-2">A ideia de empreender está associada, em geral, a pioneirismo e serviços e produtos inéditos. Mas empreendedorismo pode ser aplicado a grandes ou pequenos negócios e ONGs já existentes, introduzindo-se novas tecnologias ou ferramentas inovadoras. Ser um empreendedor vai muito além de ter o próprio negócio. É desenvolver a mentalidade de enxergar oportunidades, abraçar novas ideias e refletir sobre as novidades no mundo. Curiosidade, criatividade, resiliência, otimismo, empatia, confiança em si mesmo e responsabilidade social são características importantes para o empreendedor.
+          <p className="text-white py-2">A ideia de empreender está associada, em geral, a pioneirismo e serviços e produtos inéditos. Mas empreendedorismo pode ser aplicado a grandes ou pequenos negócios e ONGs já existentes, introduzindo-se novas tecnologias ou ferramentas inovadoras. Ser um empreendedor vai muito além de ter o próprio negócio. É desenvolver a mentalidade de enxergar oportunidades, abraçar novas ideias e refletir sobre as novidades no mundo. Curiosidade, criatividade, resiliência, otimismo, empatia, confiança em si mesmo e responsabilidade social são características importantes para o empreendedor.
 </p>
         </div>
       </div>
