a29eb48dc3a79dd511378177c5ab880ea865be37
heliosalesjr <heliosalesjr@gmail.com>
2024-06-23 18:06:39 -0300

    design updates

--- src/app/components/EmpRel.jsx
+++ src/app/components/EmpRel.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+
 
 function EmpRel() {
   return (
     <div className='max-w-5xl mx-auto mt-8'>
-      <div className='text-center align-middle max-w-3xl mx-auto p-8 border-3 rounded-xl border-primary-focus'>
-        <h1 className='text-3xl p=8 font-semibold underline decoration-primary-focus'>Qual a relação entre Empreendedorismo e Educação Financeira?
-</h1>
-        <p className='p-7'>Antes de empreender o jovem precisa adquirir conceitos básicos de Educação Financeira, além de contar com habilidades matemáticas para lidar com o dinheiro. A Educação Financeira é pré-requisito para a tomada de decisões conscientes tanto na vida pessoal quanto na profissional, além de uma poderosa maneira de aprender sobre eficiência da gestão dos recursos através de planejamento e visão de longo prazo.
-</p>
+      <div className='max-w-5xl mx-auto p-8 border-3 rounded-xl border-primary-focus md:p-12 bg-slate-200 '>
+        <div className="flex flex-col md:flex-row items-center justify-center">
+          <h1 className='text-3xl font-semibold underline decoration-primary-focus tracking-custom md:w-1/2 text-center md:text-left'>
+            Qual a relação entre Empreendedorismo e Educação Financeira?
+          </h1>
+          <p className='md:w-1/2 text-center md:text-left mt-4 md:mt-0'>
+            Antes de empreender o jovem precisa adquirir conceitos básicos de Educação Financeira, além de contar com habilidades matemáticas para lidar com o dinheiro. A Educação Financeira é pré-requisito para a tomada de decisões conscientes tanto na vida pessoal quanto na profissional, além de uma poderosa maneira de aprender sobre eficiência da gestão dos recursos através de planejamento e visão de longo prazo.
+          </p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default EmpRel
+export default EmpRel;
