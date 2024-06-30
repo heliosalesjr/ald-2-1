c05af215dc79f992d1051be6c62cec80664ddb68
heliosalesjr <heliosalesjr@gmail.com>
2024-06-29 23:33:15 -0300

    styling adjustments

--- src/app/components/EmpRel.jsx
+++ src/app/components/EmpRel.jsx
@@ -2,9 +2,9 @@
 
 function EmpRel() {
   return (
-    <div className=' mt-8 py-16 bg-slate-200'>
+    <div className='mt-8 py-16 bg-slate-200'>
       <div className='max-w-5xl mx-auto p-8 border-3 rounded-xl border-blue-400 md:p-12 bg-slate-100 '>
-        <div className="flex flex-col md:flex-row items-center justify-center">
+        <div className="flex flex-col md:flex-row items-center justify-center m-4 ">
           <h1 className='text-3xl font-semibold underline decoration-blue-400 tracking-custom md:w-1/2 text-center md:text-left'>
             Qual a relação entre Empreendedorismo e Educação Financeira?
           </h1>
